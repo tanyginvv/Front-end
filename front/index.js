04171c1a2b111a2fa096da7f6d24eed896a06096
Dmitry <99485278+DmitryIldyukov@users.noreply.github.com>
2022-10-13 21:56:34 +0300

    Add files via upload

--- front/index.js
+++ front/index.js
@@ -1,34 +1,44 @@
-const editor = {
-    presentation,
-    tools: [tool]
-}
-
-const presentation = {
-    presentationName: 'Презентация',
-    slides: [slide]
-}
-
-const slide = {
-    background: 'white',
-    blocks: [block]
-}
-
-const block = {
-    typeBlock: text | image | figure,
-    width: 1920,
-    height: 1080,
+const block1 = {
+    typeBlock: 'text',
     position: {
         x: 232,
-        y: 242
-    }
+        y: 435
+    },
+    selected: true
+}
+
+const block2 = {
+    typeBlock: 'image',
+    position: {
+        x: 400,
+        y: 200
+    },
+    selected: false
+}
+
+const block3 = {
+    typeBlock: 'figure',
+    position: {
+        x: 300,
+        y: 200
+    },
+    selected: false
 }
 
 const text = {
-    content: 'Привет',
+    ...block1,
+    content: 'hello',
     width: 400,
     height: 600,
     fontFamily: 'Roboto',
+    fontWidth: 'Bold',
+    fontSize: 24,
     color: 'black'
+}
+
+const figure = {
+    ...block2,
+    figureType: 'circle'
 }
 
 const image = {
@@ -37,43 +47,58 @@ const image = {
     src: '/study/hello.jpg'
 }
 
-const figure = {
-    figureType: rectangle | circle | triangle,
-    color: 'black',
-    borderColor: 'white'
+const slide1 = {
+    blocks: [block1, block2, block3],
+    background: "white",
+    selected: true
+}
+
+const presentation = {
+    presentationName: 'Презентация',
+    slides: [slide1]
+}
+
+const editor = {
+    presentation,
+    tools: []
 }
 
 const circle = {
-  centerPoint: {
-    x: 40,
-    y: 30
-  },
-  radiusPoint: {
-    x: 80,
-    y: 40
-  }
-}
+    ...figure,
+    centerPoint: {
+        x: 40,
+        y: 30
+    },
+    radiusPoint: {
+        x: 80,
+        y: 40
+    },
+    color: 'black',
+    borderColor: 'white'
+};
 
 const rectangle = {
-  width: 400,
-  height: 600,
-  mainPoint: {
-    x: 80,
-    y: 40
-  }
+    width: 400,
+    height: 600,
+    color: 'black',
+    borderColor: 'white',
+    mainPoint: {
+        x: 80,
+        y: 40
+    }
 }
 
 const triangle = {
-  firstPoint: {
-    x: 40,
-    y: 30
-  },
-  secondPoint: {
-    x: 80,
-    y: 40
-  },
-  thirdPoint: {
-    x: 40,
-    y: 30
-  } 
+    firstPoint: {
+        x: 40,
+        y: 30
+    },
+    secondPoint: {
+        x: 80,
+        y: 40
+    },
+    thirdPoint: {
+        x: 40,
+        y: 30
+    } 
 }
