8f4a4cb69db53dfe55c7e8e00c430ca6a0f22eca
Dmitry <99485278+DmitryIldyukov@users.noreply.github.com>
2022-10-21 10:31:32 +0300

    Add files via upload

--- front/index.js
+++ front/index.js
@@ -1,13 +1,17 @@
 const block1 = {
+    id: '1',
     typeBlock: 'text',
     position: {
         x: 232,
         y: 435
     },
+    width: 400,
+    height: 600,
     selected: true
 }
 
 const block2 = {
+    id: '2',
     typeBlock: 'image',
     position: {
         x: 400,
@@ -17,6 +21,7 @@ const block2 = {
 }
 
 const block3 = {
+    id: '3',
     typeBlock: 'figure',
     position: {
         x: 300,
@@ -28,8 +33,6 @@ const block3 = {
 const text = {
     ...block1,
     content: 'hello',
-    width: 400,
-    height: 600,
     fontFamily: 'Roboto',
     fontWidth: 'Bold',
     fontSize: 24,
@@ -42,19 +45,20 @@ const figure = {
 }
 
 const image = {
-    width: 400,
-    height: 600,
     src: '/study/hello.jpg'
 }
 
 const slide1 = {
+    id: '1',
     blocks: [block1, block2, block3],
     background: "white",
-    selected: true
+    // selected: true
 }
 
 const presentation = {
     presentationName: 'Презентация',
+    selectedSlidesIds: ['1'],
+    selectedObjectsIds: ['1', '2'],
     slides: [slide1]
 }
 
@@ -69,17 +73,12 @@ const circle = {
         x: 40,
         y: 30
     },
-    radiusPoint: {
-        x: 80,
-        y: 40
-    },
+    radius: 50,
     color: 'black',
     borderColor: 'white'
-};
+}
 
 const rectangle = {
-    width: 400,
-    height: 600,
     color: 'black',
     borderColor: 'white',
     mainPoint: {
@@ -89,16 +88,107 @@ const rectangle = {
 }
 
 const triangle = {
-    firstPoint: {
-        x: 40,
-        y: 30
-    },
-    secondPoint: {
+    mainPoin: {
         x: 80,
-        y: 40
-    },
-    thirdPoint: {
-        x: 40,
-        y: 30
-    } 
+        y: 90
+    }
 }
+
+
+
+
+function changePresentationName(editor) {
+    return presentation;
+}
+
+function deleleSlides(editor) {
+    return 0;
+}
+
+//block
+function addBlockText(editor){
+    return slide;
+}
+function addBlockImage(editor){
+    return slide;
+}
+function addBlockFigure(editor){
+    return slide;
+}
+function deleteBlock(editor){
+    return 0;
+}
+function moveBlock(block, editor){
+    return block;
+}
+
+//chars
+function editTextSize(editor){
+    return text;
+}
+function editTextColor(editor){
+    return text;
+}
+function editTextContent(editor){
+    return text;
+}
+function editTextFontFamily(editor, fontFamily){
+    return text;
+}
+function editTextFontSize(editor, fontSize){
+    return text;
+}
+function editTextFontWidth(editor, fontWidth){
+    return text;
+}
+
+//image
+function createImage(src, editor){
+    return image;
+}
+function editImageSize(image, editor){
+    return image;
+}
+
+//circle
+function createCircle(editor){
+    return circle;
+}
+function moveCircleCenter(editor, centerPoint){
+    return circle;
+}
+function editCircleRadius(editor, radius){
+    return circle;
+}
+function editCircleColor(editor, color){
+    return circle;
+}
+function editCircleBorderColor(editor, borderColor){
+    return circle;
+}
+
+//rectangle
+function createRectangle(editor){
+    return rectangle;
+}
+function editRectangleColor(editor, color){
+    return rectangle;
+}
+function editRectangleSize(editor, size){
+    return rectangle;
+}
+function editRectangleBorderColor(editor, borderColor){
+    return rectangle;
+} 
+
+//triangle
+function createTriangle(editor){
+    return triangle;
+}
+function moveTriangle(editor, centerPoint){
+    return triangle;
+}
+function editTriangleColor(editor, color){
+    return triangle;
+}
+
